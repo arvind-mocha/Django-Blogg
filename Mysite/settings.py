d90462aebda256f96c6f36c2601b91293934eedd
Arvind_mocha <arvindarvind2210@gmail.com>
2020-10-18 18:02:02 +0530

    'Done'

--- Mysite/settings.py
+++ Mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = (os.enviorn.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['my-blogg-app-django.herokuapp.com']
 
